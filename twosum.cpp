dce5ce0025f2fb9a77a0eae5e159e1a367dbac08
Arpit <arpittyagirocks@gmail.com>
2023-06-18 00:49:26 +0530

    Brute force method

--- twosum.cpp
+++ twosum.cpp
@@ -1,12 +1,32 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+vector<int> twoSum(vector<int> nums,int target){    // Bruteforce method
+
+    int size=nums.size();
+
+    for(int i=0; i<size; i++)
+        for(int j=0; j<i; j++)
+            if(nums[i]+nums[j] == target)
+                return {i,j};
+        
+    return {};
+}
+
 int main(){
-    int size;
+    int size,temp;      // Variable declaration
     vector<int> nums;
-    cin>>size;
+    
+    cin>>size;    // Inputing the values
     for(int i=0; i<size; i++){
-        cin>>
+        cin>>temp;
+        nums.push_back(temp);
     }
+    cin>>temp;
+
+    vector<int> result = twoSum(nums,temp);      // Calling the function
+
+    cout<<"The solution recieved {"<<result[0]<<","<<result[1]<<"}";        // Printing the result
     return 0;
 }
