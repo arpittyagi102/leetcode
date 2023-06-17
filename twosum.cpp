93a5be2b2ce1f2a72e89e56e3a0fc3b7d3c40f69
Arpit <arpittyagirocks@gmail.com>
2023-06-18 00:02:09 +0530

    First commit

---
+++ twosum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size;
+    vector<int> nums;
+    cin>>size;
+    for(int i=0; i<size; i++){
+        cin>>
+    }
+    return 0;
+}
